69506617714a08809358324eca751581fe2f10cb
zhoozhik <zhoozhik.us@gmail.com>
2022-09-13 20:00:46 -0500

    production

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
-import morgan from 'morgan';
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 import colors from 'colors'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRouter from './Routes/productRouter.js'
 import userRoutes from './Routes/userRoutes.js'
 import orderRoutes from './Routes/orderRoutes.js'
@@ -16,14 +16,12 @@ connectDB()
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+  app.use(morgan('dev'))
 }
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running....')
-})
+
 
 app.use('/api/products', productRouter)
 app.use('/api/users', userRoutes)
@@ -31,14 +29,31 @@ app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
-    res.send(process.env.PAYPAL_CLIENT_ID))
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
-const __dirname = path.resolve()    
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))    
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
